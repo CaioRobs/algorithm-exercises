cce1ad692ab231d82680ba0d022e7f5c6136e6ae
Caio R. R. Vieira <caio.robs98@gmail.com>
2023-08-04 15:42:46 -0300

    style: fix lint

--- src/functions/getTable.ts
+++ src/functions/getTable.ts
@@ -1,5 +1,5 @@
 export const getTable = (num: number) => {
-  if (isNaN(num)) throw new Error("Invalid number");
+  if (isNaN(num)) throw new Error('Invalid number');
 
   for (let i = 1; i <= 10; i += 1) {
     console.log(`${num} * ${i} = ${num * i}`);
--- src/functions/isPalindrome.ts
+++ src/functions/isPalindrome.ts
@@ -1,7 +1,7 @@
 export const isPalindrome = (str: string) => {
   const notAlphanumerical = /[\W_]/g;
 
-  const lowRegStr = str.toLowerCase().replace(notAlphanumerical, "");
-  var reverseStr = lowRegStr.split("").reverse().join("");
+  const lowRegStr = str.toLowerCase().replace(notAlphanumerical, '');
+  const reverseStr = lowRegStr.split('').reverse().join('');
   return reverseStr === lowRegStr;
 };
